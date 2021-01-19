f79215f373727edd9ee943da3b5e4f6d7834bd2d
harens <harensdeveloper@gmail.com>
2021-01-19 18:50:05 +0000

    Rewrite with seaport

--- scripts/main.sh
+++ scripts/main.sh
@@ -8,61 +8,31 @@ curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macp
 # shellcheck disable=SC1091
 source ./macports-ci install
 
-echo -e "\033[1;94m⬇️ Installing GitHub CLI"
-sudo port install gh
+echo -e "\033[1;94m⬇️ Installing seaport"
+# Speed up a little by not running brew cleanup
+export HOMEBREW_NO_INSTALL_CLEANUP="true"
+# If this works remove --HEAD
+brew install harens/tap/seaport --HEAD
 
 echo -e "\033[1;94m😀 Authenticating GitHub CLI"
 echo "$TOKEN" >> token.txt
 gh auth login --with-token < token.txt
 gh auth status
 
-echo -e "\033[1;94m🔍 Determining current outdated version"
-CURRENT=$(port info --version "$NAME" | sed 's/[A-Za-z: ]*//g')  # Remove letters, colon and space
-echo "Current version number is $CURRENT!"
+# For some reason the gh repo fork in seaport is too slow
+# This clones it before hand using the pre-seaport method
 
-echo -e "\033[1;94m🏷️ Determining new version from tag"
-# We want to keep the dots, but remove all letters
-TAG="${TAG//[A-Za-z]}"
-echo "New version number is $TAG!"
-
-echo -e "\033[1;94m📒 Determining Category"
-CATEGORY=$(port info --category "$NAME")
-# Only take the first category
-# Replace commas with line breaks
-CATEGORY=$(echo "$CATEGORY" | cut -d' ' -f2 | tr "," "\n")
-echo "Category is $CATEGORY!"
-
+# Homebrew doesn't like the USER variable being set
 echo -e "\033[1;94m⬇️ Cloning MacPorts Repo"
-# Can't authenticate by cloning through gh repo
-gh repo fork "$REPO" --clone=false --remote=true
-# Name of the cloned folder
-CLONE=$(echo "$REPO" | awk -F'/' '{print $2}')
-git clone https://"$USER":"$TOKEN"@github.com/"$USER"/"$CLONE"
-# git clone https://"$USER":"$TOKEN"@github.com/"$REPO"
 
-echo -e "\033[1;94m📁 Creating local Portfile Repo"
-mkdir -p ports/"$CATEGORY"/"$NAME"
-# Copy Portfile to the local repo
-cp "$CLONE"/"$CATEGORY"/"$NAME"/Portfile ports/"$CATEGORY"/"$NAME"/Portfile
-# shellcheck disable=SC1091
-source ./macports-ci localports ports
+# All of this should be unnecessary...but it isn't for some reason
 
-echo -e "\033[1;94m⏫ Bumping Version"
-# Replaces first instance of old version with new version
-sed -i '' "1,/$CURRENT/ s/$CURRENT/$TAG/" ports/"$CATEGORY"/"$NAME"/Portfile
-sudo port bump "$NAME"
+gh repo fork macports/macports-ports
+git clone https://"$GITHUB_USER":"$TOKEN"@github.com/"$GITHUB_USER"/macports-ports
 
-echo -e "\033[1;94m📨 Sending PR"
+cd macports-ports || exit 1
+git remote add upstream "https://github.com/macports/macports-ports"
+cd ..
 
-cd "$CLONE" || exit 1
-git checkout -b "bump-$NAME-$TAG"
-# Copy changes back to main repo
-cp ../ports/"$CATEGORY"/"$NAME"/Portfile "$CATEGORY"/"$NAME"/Portfile
-git add "$CATEGORY"/"$NAME"/Portfile
-git commit -m "$NAME: update to $TAG"
-git remote add upstream "https://github.com/$REPO"
-git config "remote.upstream.gh-resolved" "base"
-git push --set-upstream origin "bump-$NAME-$TAG"
-# git remote set-url origin "https://github.com/$REPO"  # Create PR to upstream
-gh pr create --title "$NAME: update to $TAG" --body "Created with [action-macports-bump](https://github.com/harens/action-macports-bump)" --head="bump-$NAME-$TAG"
-echo -e "\033[1;94m🎉 PR Sent!"
+# Uses the current directory
+seaport pr "$NAME" "$(pwd)"
